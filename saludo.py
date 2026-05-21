f5af8f2335955508fcec519d499a55ea7e4e72ee
eliannishernandez09 <eliannissantiago09@gmail.com>
2026-05-21 15:56:56 -0500

    feat: agrego saludo.py

---
+++ saludo.py
@@ -0,0 +1,10 @@
+# saludo.py
+# Completá con tus datos reales
+
+nombre  = "Eliannis Hernandez"
+carrera = "Análisis y Desarrollo de Software"
+proyecto = "Proyecto"
+
+print(f"Hola, soy {nombre}")
+print(f"Estudio {carrera} en el SENA")
+print(f"Mi proyecto es: {proyecto}")
